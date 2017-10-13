4c44b86358a0be37aa31b6caeb88847be186e20e
shawgum <shawgum@gmail.com>
2017-10-13 17:01:32 +0200

    initial commit

---
+++ CrackRAR/CrackRAR/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using static CrackRAR.UnRAR;
+
+namespace CrackRAR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            UnRAR.RAROpenArchiveDataEx openData = new UnRAR.RAROpenArchiveDataEx
+            {
+                ArcName = null,
+                ArcNameW = "password1234.rar" + '\0',
+                OpenMode = 0,
+                Callback = null,
+                UserData = new IntPtr(123)
+            };
+
+            var handle = RAROpenArchiveEx(ref openData);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CrackRAR/CrackRAR/UnRAR.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrackRAR
+{
+    public class UnRAR
+    {
+
+        public enum OpenMode : uint
+        {
+            RAR_OM_LIST,
+            RAR_OM_EXTRACT,
+            RAR_OM_LIST_INCSPELIT
+        }
+
+        public enum OpenResult : uint
+        {
+            SUCCESS = 0,
+            ERAR_NO_MEMORY,
+            ERAR_BAD_DATA,
+            ERAR_UNKNOWN_FORMAT,
+            ERAR_EOPEN,
+            ERAR_BAD_PASSWORD
+        }
+
+        [DllImport("UnRAR64.dll")]
+        public static extern IntPtr RAROpenArchiveEx(ref RAROpenArchiveDataEx archiveData);
+
+        [DllImport("UnRAR64.dll")]
+        public static extern int RARCloseArchive(IntPtr hArcData);
+
+        [DllImport("UnRAR64.dll")]
+        public static extern int RARReadHeaderEx(IntPtr hArcData, RARHeaderDataEx HeaderData);
+
+        [DllImport("UnRAR64.dll")]
+        public static extern int RARProcessFileW(IntPtr hArcData, int Operation, string DestPath, string DestName);
+
+        //[DllImport("UnRAR64.dll")]
+        //public static extern int CallbackProc(uint msg, IntPtr UserData, IntPtr P1, IntPtr P2);
+
+        public delegate int UnRARCallBack(uint msg, IntPtr UserData, IntPtr P1, IntPtr P2);
+
+        public struct RAROpenArchiveDataEx
+        {
+            public string ArcName;
+            [MarshalAs(UnmanagedType.LPWStr)]
+            public string ArcNameW;
+            public uint OpenMode;
+            public uint OpenResult;
+            public string CmtBuf;
+            public uint CmtBufSize;
+            public uint CmtSize;
+            public uint CmtState;
+            public uint Flags;
+            public UnRARCallBack Callback;
+            public IntPtr UserData;
+            public uint[] Reserved;
+        };
+
+        public struct RARHeaderDataEx
+        {
+            string ArcName;
+            string ArcNameW;
+            char FileName;
+            string FileNameW;
+            uint Flags;
+            uint PackSize;
+            uint PackSizeHigh;
+            uint UnpSize;
+            uint UnpSizeHigh;
+            uint HostOS;
+            uint FileCRC;
+            uint FileTime;
+            uint UnpVer;
+            uint Method;
+            uint FileAttr;
+            string CmtBuf;
+            uint CmtBufSize;
+            uint CmtSize;
+            uint CmtState;
+            uint DictSize;
+            uint HashType;
+            string Hash;
+            uint RedirType;
+            string RedirName;
+            uint RedirNameSize;
+            uint DirTarget;
+            uint MtimeLow;
+            uint MtimeHigh;
+            uint CtimeLow;
+            uint CtimeHigh;
+            uint AtimeLow;
+            uint AtimeHigh;
+            uint[] Reserved;
+        };
+    }
+}
